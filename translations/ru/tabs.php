a2be7356f3e65bac9a731237a8e1d6536e6e1ad6
Alexander <bartworkmail@gmail.com>
2014-04-14 14:10:08 +0300

    Create tabs.php

---
+++ translations/ru/tabs.php
@@ -0,0 +1,28 @@
+<?php
+
+$_TABS = array();
+
+$_TABS['AdminQuickAccesses'] = 'Быстрый доступ';
+$_TABS['AdminEmployees'] = 'Сотрудники';
+$_TABS['AdminTabs'] = 'Закладки';
+$_TABS['AdminStatuses'] = 'Статусы';
+$_TABS['AdminModulesPositions'] = 'Расположение блоков';
+$_TABS['AdminReferrers'] = 'Рефереры';
+$_TABS['AdminGroups'] = 'Группы';
+$_TABS['AdminContacts'] = 'Контакты';
+$_TABS['AdminGenders'] = 'Формы обращения';
+$_TABS['AdminSuppliers'] = 'Поставщики';
+$_TABS['AdminTags'] = 'Тэги';
+$_TABS['AdminAttachments'] = 'Загрузить';
+$_TABS['AdminLogin'] = 'Логин';
+$_TABS['AdminWarehouses'] = 'Склады';
+$_TABS['AdminLanguages'] = 'Язык';
+$_TABS['AdminZones'] = 'Зоны';
+$_TABS['AdminCountries'] = 'Страны';
+$_TABS['AdminStates'] = 'Регионы';
+$_TABS['AdminTaxRulesGroup'] = 'Налоговые нормы';
+$_TABS['AdminCartRules'] = 'Правила корзины';
+$_TABS['AdminCarrierWizard'] = 'Способ доставки';
+
+
+return $_TABS;
