02b229cac5f424538a4358342b0f3aa31bbfa3e1
Alexander <bartworkmail@gmail.com>
2014-04-14 14:09:50 +0300

    Create pdf.php

---
+++ translations/ru/pdf.php
@@ -0,0 +1,47 @@
+<?php
+
+global $_LANGPDF;
+$_LANGPDF = array();
+
+$_LANGPDF['PDF2b56b5ec383be252fe778c4c6066b2fb'] = 'Адрес доставки и выставления счетов.';
+$_LANGPDF['PDF5e5f0a863cd129317037e3def37b4097'] = 'Название:';
+$_LANGPDF['PDF197101c4a1b1fc503dcd6ebee127aa10'] = 'Цена';
+$_LANGPDF['PDF078ac079d154d21533c57bced9fcb10f'] = '(Без НДС)';
+$_LANGPDF['PDF104d9898c04874d0fbac36e125fa1369'] = 'Скидка';
+$_LANGPDF['PDF03ab340b3f99e03cff9e84314ead38c0'] = 'Кол-во';
+$_LANGPDF['PDF96b0141273eabab320119c467cdcaf17'] = 'Итого';
+$_LANGPDF['PDF3dd42bfc8345e82c43b4a40a3426ec49'] = 'Артикул';
+$_LANGPDF['PDF5082245495aa792b3d0874088d2b111b'] = 'Всего за товары';
+$_LANGPDF['PDF19abe6c02a26dcd93020396df8d0721c'] = 'Итого купонов:';
+$_LANGPDF['PDFced2da7b2baf942c0ffd4b8e45ec0fad'] = 'Стоимость упаковки';
+$_LANGPDF['PDFa5e35abc0c9e2d2784d0ef619b36448b'] = 'Стоимость доставки';
+$_LANGPDF['PDFb602e0d0c6a72053d0a5be60cb2f8126'] = 'Итого налогов';
+$_LANGPDF['PDF023f66b09075fa1cc29e74bed9f5ac40'] = 'Подробности о налоге';
+$_LANGPDF['PDF8fe77c2601e54f1aaef28cfde997bbad'] = 'Ставка налога';
+$_LANGPDF['PDF22caf639b05c0fa192e81d58c553c89a'] = 'Всего, не включая налоги';
+$_LANGPDF['PDFe92cfa244b5eb9025d07522080468445'] = 'Эконалог';
+$_LANGPDF['PDF29103375229a5c363403ec4afb5c3e8f'] = '(Налог вкл.)';
+$_LANGPDF['PDF1d744a9ad1dac20645cfc4a36b77323b'] = 'изображение(й)';
+$_LANGPDF['PDF752ab8bad7c2e2d894ffa7f2b4f59919'] = 'Возвратный номер:';
+$_LANGPDF['PDF0d8d58008ef5dd7afce337373ef73993'] = 'Дата';
+$_LANGPDF['PDF3601146c4e948c32b6424d2c0a7f0118'] = 'Цена';
+$_LANGPDF['PDF09466661df2abefeecf849e52fce8fb4'] = 'Итого';
+$_LANGPDF['PDF63d5049791d9d79d86e9a108b0a999ca'] = 'Артикул';
+$_LANGPDF['PDFf107f5a9ee4ecea58d0f29ae7959baa8'] = 'Маркировка';
+$_LANGPDF['PDFc8ba77264f6e923f5ee35abf3108ae7c'] = 'Цена за единицу (без НДС)';
+$_LANGPDF['PDF3a6a6d37bea2c7567f8599ea88b47930'] = 'Всего (без НДС)';
+$_LANGPDF['PDF7521172dd37ba56442ea866ac18eb49d'] = 'До скидки';
+$_LANGPDF['PDF6703aa9936582b4381418f7d523370b4'] = 'Уровень скидки';
+$_LANGPDF['PDFa61109b693023aed557445989c517982'] = 'После скидки';
+$_LANGPDF['PDF20a34c4e30c5bbe1d3f870ac55f0d831'] = 'Ставка налога';
+$_LANGPDF['PDFfa2ce544158555e8f06c69dd9e681832'] = 'Всего (вкл. НДС)';
+$_LANGPDF['PDF716be2c7de31f063369ca297a9928403'] = 'Налоги';
+$_LANGPDF['PDFf4a232c48df10124ee867767a4e7280e'] = 'Основа (без НДС)';
+$_LANGPDF['PDF01a2436b8e3782c1f99e6b26adf901b2'] = 'Налог';
+$_LANGPDF['PDF1300f86974cafc2d65eb1f082a26bdf1'] = 'Выбор';
+$_LANGPDF['PDF6b9e9f2472786ed1e76005bfc37bc8af'] = 'Скидка заказа';
+$_LANGPDF['PDF699e6bd4690ed3bd5f658052e12aba04'] = 'Налог';
+$_LANGPDF['PDF448634536559b1f8aaa975e0aa12b930'] = 'ИТОГО К ОПЛАТЕ';
+
+
+return $_LANGPDF;
